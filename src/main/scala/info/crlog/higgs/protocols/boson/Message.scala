69b30e8d2f46560469712ed5e20e92bf3b0bb872
Courtney Robinson <courtney@crlog.info>
2012-11-10 19:23:33 +0000

    add protcol version to message making it possible to switch protocols on a per msg basis

--- src/main/scala/info/crlog/higgs/protocols/boson/Message.scala
+++ src/main/scala/info/crlog/higgs/protocols/boson/Message.scala
@@ -1,8 +1,14 @@
 package info.crlog.higgs.protocols.boson
 
 /**
+ * Represents a response or request
+ * @param method   the method name to invoke when this represents a request
+ * @param arguments the arguments to be passed to the method in both requests and responses
+ * @param callback  the name of the client fallback function to be invoked when a response is received.
+ *                  If empty this is assumed to be a response.
+ * @param protocolVersion A 16 bit int
  * @author Courtney Robinson <courtney@crlog.info>
  */
-case class Message(method: String, arguments: Array[Any] = Array(), callback: String = "") {
+case class Message(method: String, arguments: Array[Any] = Array(), callback: String = "", protocolVersion: Short = 0x1) {
   def this() = this("", Seq()) //required for instantiation via reflection
 }
