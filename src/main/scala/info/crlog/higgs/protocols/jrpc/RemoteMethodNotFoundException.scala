a0c53ade613141414dc12de0bc938a91bed87bd6
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:44:36 +0000

    extend runtime exception

--- src/main/scala/info/crlog/higgs/protocols/jrpc/RemoteMethodNotFoundException.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/RemoteMethodNotFoundException.scala
@@ -3,6 +3,4 @@ package info.crlog.higgs.protocols.jrpc
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-class RemoteMethodNotFoundException {
-
-}
+class RemoteMethodNotFoundException(msg: String) extends RuntimeException(msg)
