057d11d53794bda9ff4306ebbb0a9ea9b9fafdc8
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:45:49 +0000

    implementation of encoder and decoder

--- src/main/scala/info/crlog/higgs/protocols/jrpc/RPCDecoder.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/RPCDecoder.scala
@@ -1,8 +1,30 @@
 package info.crlog.higgs.protocols.jrpc
 
+import io.netty.handler.codec.ByteToMessageDecoder
+import io.netty.channel.ChannelHandlerContext
+import io.netty.buffer.ByteBuf
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-class RPCDecoder {
+class RPCDecoder extends ByteToMessageDecoder[Array[Byte]] {
 
+  def decode(ctx: ChannelHandlerContext, buffer: ByteBuf): Array[Byte] = {
+    // Wait until the size of the message is available.
+    if (buffer.readableBytes < 4) {
+      return null
+    }
+    //set reader index to 0
+    buffer.markReaderIndex
+    val dataLength: Int = buffer.readInt //get the data size, i.e. first 4 bytes (32 bit signed java int)
+    // Wait until all the full message is available
+    if (buffer.readableBytes < dataLength) {
+      buffer.resetReaderIndex
+      return null
+    }
+    // get the data.
+    val messageContents: Array[Byte] = new Array[Byte](dataLength)
+    buffer.readBytes(messageContents)
+    messageContents
+  }
 }
--- src/main/scala/info/crlog/higgs/protocols/jrpc/RPCEncoder.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/RPCEncoder.scala
@@ -1,8 +1,15 @@
 package info.crlog.higgs.protocols.jrpc
 
+import io.netty.handler.codec.MessageToByteEncoder
+import io.netty.channel.ChannelHandlerContext
+import io.netty.buffer.ByteBuf
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-class RPCEncoder {
-
+class RPCEncoder extends MessageToByteEncoder[Array[Byte]] {
+  def encode(ctx: ChannelHandlerContext, msg: Array[Byte], out: ByteBuf) {
+    out.writeInt(msg.length) // data length, i.e first 4 bytes ( an Int's 32 bits)
+    out.writeBytes(msg) //now write the packed message's bytes
+  }
 }
