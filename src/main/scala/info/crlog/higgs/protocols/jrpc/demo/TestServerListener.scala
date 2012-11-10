001a682871e9e8112bea24b1db703be97f192c79
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:43:36 +0000

    simple test methods for the @method annotation

--- src/main/scala/info/crlog/higgs/protocols/jrpc/demo/TestServerListener.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/demo/TestServerListener.scala
@@ -1,8 +1,48 @@
 package info.crlog.higgs.protocols.jrpc.demo
 
+import info.crlog.higgs.method
+import io.netty.channel.Channel
+import info.crlog.higgs.protocols.jrpc.RPC
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
 class TestServerListener {
 
+  @method
+  def test(a: String) {
+    println("test called with param %s" format (a))
+  }
+
+  @method("get_user")
+  def getUser(a: String) = {
+    println("get user called %s" format (a))
+    true
+  }
+
+  @method("channel")
+  def channel(a: Channel) {
+    println("channel %s" format (a))
+  }
+
+  @method("rpc")
+  def rpc(a: RPC) {
+    println("rpc %s" format (a))
+  }
+
+  @method("rpcchannel")
+  def rpcchannel(a: Channel, rpc: RPC) {
+    println("rpcchannel %s" format (a))
+  }
+
+  @method("mixchannel")
+  def mixchannel(a: Channel, rpc: RPC, name: String) {
+    println("mixchannel %s" format (a))
+  }
+
+  @method("test_response")
+  def testResponse(i: Int) = {
+    println(i)
+    100L
+  }
 }
