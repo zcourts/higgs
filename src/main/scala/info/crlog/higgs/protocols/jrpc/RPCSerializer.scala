14babf16d6939afbd58c14d9982e3fc956bb8302
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:45:10 +0000

    moved to jrpc package

--- src/main/scala/info/crlog/higgs/protocols/jrpc/RPCSerializer.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/RPCSerializer.scala
@@ -2,7 +2,6 @@ package info.crlog.higgs.protocols.jrpc
 
 import info.crlog.higgs.Serializer
 import info.crlog.higgs.protocols.omsg.SerializablePacker
-import info.crlog.higgs.protocols.jrpc.RPC
 
 /**
  * @author Courtney Robinson <courtney@crlog.info>
