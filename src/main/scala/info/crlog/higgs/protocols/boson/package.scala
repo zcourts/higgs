e8f0ffe9d13b6b3a1d3e5a8972f76e56782fe9a3
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:47:14 +0000

    package info

--- src/main/scala/info/crlog/higgs/protocols/boson/package.scala
+++ src/main/scala/info/crlog/higgs/protocols/boson/package.scala
@@ -1,6 +1,8 @@
 package info.crlog.higgs.protocols
 
 /**
+ * Boson is a language independent protocol for Higgs.
+ * It
  * @author Courtney Robinson <courtney@crlog.info>
  */
 package object boson {
