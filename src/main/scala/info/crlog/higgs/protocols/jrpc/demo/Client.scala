5f278429af615ed1dcf14c583cacc3ab8ac3701f
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:44:12 +0000

    moved to demo package

--- src/main/scala/info/crlog/higgs/protocols/jrpc/demo/Client.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/demo/Client.scala
@@ -1,8 +1,36 @@
 package info.crlog.higgs.protocols.jrpc.demo
 
+import info.crlog.higgs.protocols.jrpc.RPCClient
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-class Client {
-
+object Client {
+  def main(args: Array[String]) {
+    val client = new RPCClient("localhost", 12000)
+    client.connect(() => {
+      client.invoke("test_response", (o: Option[Any], e: Option[Throwable]) => {
+        println(o)
+      },1)
+      //      client.invoke("info.crlog.higgs.protocols.jrpc.demo.TestServerListener.test",
+      //        (r: Option[AnyRef], e: Option[Throwable]) => {
+      //          println(r, e)
+      //        }, "Courtney")
+      //      client.invoke("get_user", (r: Option[AnyRef], e: Option[Throwable]) => {
+      //        println(r, e)
+      //      }, "zcourts")
+      //      client.invoke("channel", (r: Option[AnyRef], e: Option[Throwable]) => {
+      //        println(r, e)
+      //      })
+      //      client.invoke("rpcchannel", (r: Option[AnyRef], e: Option[Throwable]) => {
+      //        println(r, e)
+      //      })
+      //      client.invoke("rpc", (r: Option[AnyRef], e: Option[Throwable]) => {
+      //        println(r, e)
+      //      })
+      client.invoke("mixchannel", (r: Option[AnyRef], e: Option[Throwable]) => {
+        println(r, e)
+      }, "name")
+    })
+  }
 }
--- src/main/scala/info/crlog/higgs/protocols/jrpc/demo/Server.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/demo/Server.scala
@@ -1,8 +1,15 @@
 package info.crlog.higgs.protocols.jrpc.demo
 
+import info.crlog.higgs.protocols.jrpc.JRPCServer
+
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
 object Server {
-
+  def main(args: Array[String]) {
+    val server = new JRPCServer("localhost", 12000)
+    server.registerPackage(getClass.getPackage.getName)
+    server.bind(() => {})
+  }
 }
