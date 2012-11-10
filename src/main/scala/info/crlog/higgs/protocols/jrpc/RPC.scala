136be1df9c83e0829058638316a625211f9523bd
Courtney Robinson <courtney@crlog.info>
2012-11-10 14:46:39 +0000

    rpc implementation used to represent requests and responses

--- src/main/scala/info/crlog/higgs/protocols/jrpc/RPC.scala
+++ src/main/scala/info/crlog/higgs/protocols/jrpc/RPC.scala
@@ -3,10 +3,28 @@ package info.crlog.higgs.protocols.jrpc
 import java.io.Serializable
 
 /**
+ * Represents a remote procedure call, both on the server and client.
+ * If the RPC is a request to the server then the isRequest field should be
+ * set to true by the client otherwise its false.
+ * @param remoteMethodName
+ * @param clientCallbackID
+ * @param arguments
+ * @param response
+ * @param error
  * @author Courtney Robinson <courtney@crlog.info>
  */
 case class RPC(remoteMethodName: String,
-                      clientCallbackID: String,
-                      remoteParams: Seq[Serializable]) extends Serializable {
-
+               clientCallbackID: String,
+               arguments: Seq[Serializable],
+               response: Option[Serializable]=None,
+               error: Option[Throwable] = None) extends Serializable {
+  /**
+   * Create an RPC with new arguments specified by args* and copy all other fields
+   * from the given RPC instance
+   * @param rpc
+   * @param args
+   */
+  def this(rpc: RPC, args: Seq[Serializable]) = {
+    this(rpc.remoteMethodName, rpc.clientCallbackID, args)
+  }
 }
