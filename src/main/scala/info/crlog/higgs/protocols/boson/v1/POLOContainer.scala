76cd3ac5af0325697d49b8cb0cb604d3dbdac0c8
Courtney Robinson <courtney@crlog.info>
2012-11-12 15:03:07 +0000

    add type checking for possible POLOs and abstracted object to POLO where both client and server can use it

--- src/main/scala/info/crlog/higgs/protocols/boson/v1/POLOContainer.scala
+++ src/main/scala/info/crlog/higgs/protocols/boson/v1/POLOContainer.scala
@@ -1,6 +1,8 @@
 package info.crlog.higgs.protocols.boson.v1
 
 import java.lang.reflect.Field
+import java.{util, lang}
+import info.crlog.higgs.protocols.boson.IllegalBosonArgumentException
 
 /**
  * POLOs are deserialized into a map.
@@ -41,4 +43,73 @@ case class POLOContainer(fields: Map[String, Any]) {
     instance
   }
 
+  /**
+   * Check if the given object is a possible POLO.
+   * An object is a possible POLO if it is NOT an instance of one of the other supported Boson data types
+   * @param obj
+   * @return
+   */
+  def isPossiblePOLO(obj: Class[_]): Boolean = {
+    if (obj == classOf[Byte] || obj == classOf[lang.Byte]) {
+      return false
+    } else if (obj == classOf[Short] || obj == classOf[lang.Short]) {
+      return false
+    } else if (obj == classOf[Int] || obj == classOf[lang.Integer]) {
+      return false
+    } else if (obj == classOf[Long] || obj == classOf[lang.Long]) {
+      return false
+    } else if (obj == classOf[Float] || obj == classOf[lang.Float]) {
+      return false
+    } else if (obj == classOf[Double] || obj == classOf[lang.Double]) {
+      return false
+    } else if (obj == classOf[Boolean] || obj == classOf[lang.Boolean]) {
+      return false
+    } else if (obj == classOf[Char] || obj == classOf[lang.Character]) {
+      return false
+    } else if (obj == classOf[String] || obj == classOf[lang.String]) {
+      return false
+    } else if (obj.isArray ||
+      classOf[Array[Any]].isAssignableFrom(obj)
+    ) {
+      return false
+    } else if (classOf[List[Any]].isAssignableFrom(obj)
+      || classOf[util.List[Any]].isAssignableFrom(obj)) {
+      return false
+    } else if (classOf[collection.Map[Any, Any]].isAssignableFrom(obj)
+      || classOf[util.Map[Any, Any]].isAssignableFrom(obj)) {
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+/**
+ * this is just here as a class because both the server and client uses the same logic and
+ * when it changes in the future this provides a central place to update.
+ * If the given param is an instance of POLOContainer, that means the method argument given
+ * is a POLO and should be replaced with the parameter field of this class.
+ * Use isPOLO to test if the given param is in fact a POLO
+ * @param param
+ * @param methodArgument
+ */
+class POLOContainerToType(param: Any, methodArgument: Class[_]) {
+  var isPOLO = false
+  //if its a polo
+  val parameter: AnyRef =
+    if (param.isInstanceOf[POLOContainer]) {
+      val polo = param.asInstanceOf[POLOContainer]
+      //make sure the method argument is not a boson primitive
+      if (polo.isPossiblePOLO(methodArgument)) {
+        val tmp = polo.as(methodArgument, true)
+        isPOLO = true
+        tmp.asInstanceOf[AnyRef]
+      } else {
+        //seeing as we have a POLOContainer and a boson supported data type
+        throw new IllegalBosonArgumentException("POLO found where %s is expected"
+          format (methodArgument.getName()), null)
+      }
+    } else {
+      param.asInstanceOf[AnyRef]
+    }
 }
