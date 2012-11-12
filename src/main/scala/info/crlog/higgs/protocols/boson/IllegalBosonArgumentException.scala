c0d603089f528dd5b321b25d047d42c2cbe8314f
Courtney Robinson <courtney@crlog.info>
2012-11-12 15:00:35 +0000

    excpetion to throw when POLO is found where other type is expected

---
+++ src/main/scala/info/crlog/higgs/protocols/boson/IllegalBosonArgumentException.scala
@@ -0,0 +1,8 @@
+package info.crlog.higgs.protocols.boson
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+class IllegalBosonArgumentException(msg: String, cause: Throwable) extends RuntimeException(msg, cause) {
+
+}
