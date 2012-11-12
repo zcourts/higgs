9bf0ea47823ae8375c96d9ae70817a024771e7d2
Courtney Robinson <courtney@crlog.info>
2012-11-12 15:03:31 +0000

    implemented client handling for POLOs

--- src/main/scala/info/crlog/higgs/protocols/boson/BosonClient.scala
+++ src/main/scala/info/crlog/higgs/protocols/boson/BosonClient.scala
@@ -1,6 +1,6 @@
 package info.crlog.higgs.protocols.boson
 
-import v1.BosonSerializer
+import v1.{POLOContainerToType, BosonSerializer}
 import info.crlog.higgs.Client
 import io.netty.channel.{Channel, ChannelHandlerContext}
 import info.crlog.higgs.util.Algorithms
@@ -84,16 +84,26 @@ class BosonClient(serviceName: String, port: Int, host: String = "localhost", co
       } else {
         //if we have in fact received parameters to be passed to the callback
         if (m.arguments.length > 0) {
-          val param = m.arguments(0)
-          val klass = param.asInstanceOf[AnyRef].getClass()
-          //if param is the same as or is a super class of the expected type, we can cast to it
-          if (mf.erasure.isAssignableFrom(klass)) {
-            callback(Some(param.asInstanceOf[T]), None)
-          } else {
-            val logmsg = "Remote method %s invoked, callback %s, " +
-              "%s cannot be cast to %s" format(method, id, klass.getName(), mf.erasure.getName())
-            log.warn(logmsg)
-            callback(None, Some(new InvalidBosonResponse(logmsg, m, null)))
+          try {
+            val param = new POLOContainerToType(m.arguments(0), mf.erasure)
+            //param.parameter is either the object m.arguments(0) OR a POLOContainer converted to a type of ms.erasure
+            val klass = param.parameter.getClass()
+            //if param is the same as or is a super class of the expected type, we can cast to it
+            if (mf.erasure.isAssignableFrom(klass)) {
+              callback(Some(param.parameter.asInstanceOf[T]), None)
+            } else {
+              val logmsg = "Remote method %s invoked, callback %s, " +
+                "%s cannot be cast to %s" format(method, id, klass.getName(), mf.erasure.getName())
+              log.warn(logmsg)
+              callback(None, Some(new InvalidBosonResponse(logmsg, m, null)))
+            }
+          } catch {
+            case e: IllegalBosonArgumentException => {
+              val logmsg = "Remote method %s invoked, callback %s, Response cannot be converted to expected type"
+                .format(method, id)
+              log.warn(logmsg, e)
+              callback(None, Some(new InvalidBosonResponse(logmsg, m, e)))
+            }
           }
         } else {
           log.info("Remote method %s invoked, acknowledgement" +
