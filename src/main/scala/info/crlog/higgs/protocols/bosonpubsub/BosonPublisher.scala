f6db46b45fc879aa94d3156b1ba1891d486f152f
Courtney Robinson <courtney@crlog.info>
2012-11-20 11:23:45 +0000

    starting pubsub APIs

--- src/main/scala/info/crlog/higgs/protocols/bosonpubsub/BosonPublisher.scala
+++ src/main/scala/info/crlog/higgs/protocols/bosonpubsub/BosonPublisher.scala
@@ -1,8 +1,13 @@
 package info.crlog.higgs.protocols.bosonpubsub
 
+import info.crlog.higgs.protocols.boson.BosonServer
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-class BosonPublisher {
+class BosonPublisher(port: Int, host: String = "localhost", compress: Boolean = false)
+  extends BosonServer(port, host, compress) {
+  def send() {
 
+  }
 }
