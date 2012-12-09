ecff2de7039a173beec8cd6331c7ab025f8c17ba
Courtney Robinson <courtney@crlog.info>
2012-12-09 04:03:23 +0000

    set of supported events

---
+++ higgs-core/src/main/java/com/fillta/higgs/events/HiggsEvent.java
@@ -0,0 +1,13 @@
+package com.fillta.higgs.events;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public enum HiggsEvent {
+    CHANNEL_ACTIVE,
+    CHANNEL_INACTIVE,
+    CHANNEL_REGISTERED,
+    CHANNEL_UNREGISTERED,
+    EXCEPTION_CAUGHT,
+    MESSAGE_RECEIVED
+}
