395f8789beb38c068c6a7159a77afdd220cfbb3e
Courtney Robinson <courtney@crlog.info>
2012-12-09 04:04:51 +0000

    various queueing strategies used for messsage processing

---
+++ higgs-core/src/main/java/com/fillta/higgs/queueingStrategies/QueueingStrategy.java
@@ -0,0 +1,150 @@
+package com.fillta.higgs.queueingStrategies;
+
+import com.fillta.higgs.MessageTopicFactory;
+import com.fillta.higgs.events.ChannelMessage;
+import com.fillta.higgs.util.Function1;
+import com.google.common.collect.ArrayListMultimap;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This package exists largely because of an observation in the Scala implementation.
+ * When messages are received or sent they need to be off loaded and processed.
+ * If processing 1 message takes 1 second or even 1 millisecond when there are hundreds of thousands of messages to
+ * be processed, this can take time and cause a backlog.
+ * Expirments with queues of all types showed that even if messages are "buffered"/queued and processed with multiple
+ * threads, the queue then becomes the bottle neck. In some observations various {@link java.util.Queue} implementations
+ * spend more time locking than the threads do processing, i.e. the contention rate sky rockets!
+ * <p/>
+ * Different approaches yield different results, performance and "safety" wise.
+ * <p/>
+ * By default {@link com.fillta.higgs.EventProcessor} uses the {@link SameThreadQueueingStrategy} which processes
+ * messages on the same thread the arrived on. This is as safe is it gets but has the obvious drawback that if while
+ * processing one of those messages a thread gets blocked, you're blocking the Netty worker thread, blocking all of
+ * them could render the {@link com.fillta.higgs.EventProcessor} useless.
+ * <p/>
+ * The {@link LinkedBlockingQueueStrategy} uses Java's {@link java.util.concurrent.ConcurrentLinkedQueue} to buffer
+ * messages, it then uses the {@link java.util.concurrent.ForkJoinPool} and {@link java.util.concurrent.RecursiveAction}
+ * to process messages from the queue. This gets rid of the chances of you blocking the Netty threads but introduces
+ * a high contention ratio with multiple {@link java.util.concurrent.RecursiveAction}s reading from the queue.
+ * <p/>
+ * {@link CircularBufferQueueingStrategy} uses our {@link com.fillta.higgs.buffer.CircularBuffer} implementation.
+ * See <a href="http://en.wikipedia.org/wiki/Circular_buffer">http://en.wikipedia.org/wiki/Circular_buffer</a>
+ * for an overview and <a href="http://www.youtube.com/watch?v=DCdGlxBbKU4">http://www.youtube.com/watch?v=DCdGlxBbKU4</a>
+ * for the basis of our implementation.
+ * <p/>
+ * So, the biggest risk with using the circular buffer is the possibility of losing messages. A circular buffer by its nature
+ * writes stuff into its backing array until it is full. Once full, it starts overwriting the oldest items in the buffer.
+ * By default {@link com.fillta.higgs.buffer.CircularBuffer} initializes its backing array to 1 million. This should
+ * be enough for most cases and results in about 20MB of memory being used just for the empty array;
+ * <p/>
+ * The advantage, the {@link java.util.concurrent.ForkJoinPool} can be used with a {@link java.util.concurrent.RecursiveAction}
+ * with little to no read contention by multiple processing threads. This significantly speeds up message processing
+ * as the buffer does not lock, so the processing threads do not contend when fetching items from the buffer.
+ * If memory is not an issue the circular buffer can easily have its backing array size increased. However, if an application
+ * is having 1M messages queued in memory chances are increasing the array size won't do much good since you're just going
+ * to hit that larger size anyway, optimization is probably needed in the application itself. Or simply increasing
+ * the number of message processing threads. Not too much, or it'll just lead to too much context switching, that
+ * in itself being counter productive and becoming somewhat of a bottleneck.
+ * <p/>
+ * <p/>
+ * A queueing strategy allows different {@link com.fillta.higgs.EventProcessor}s to handle messages in different ways.
+ * For example, the {@link CircularBufferQueueingStrategy} pops messages in a
+ * {@link com.fillta.higgs.buffer.CircularBuffer} - This allows 1 thread to write to the buffer
+ * while multiple threads consume items from the buffer without contention.
+ * The {@link LinkedBlockingQueueStrategy} uses a {@link java.util.concurrent.ConcurrentLinkedQueue}
+ * which is safer but increases contention amongst threads reading and/or writing to it.
+ * The {@link SameThreadQueueingStrategy} invokes listeners on the calling thread, i.e. the Netty
+ * threads. This works, it is simple but if listeners block it can cause issues.
+ *
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public abstract class QueueingStrategy<T, IM> {
+    public final ArrayListMultimap<T, Function1<ChannelMessage<IM>>> messageSubscribers = ArrayListMultimap.create();
+    public final Set<Function1<ChannelMessage<IM>>> allMessageSubscribers = new HashSet<>();
+    protected MessageTopicFactory<T, IM> topicFactory;
+    protected Logger log = LoggerFactory.getLogger(getClass());
+
+    public QueueingStrategy(MessageTopicFactory<T, IM> topicFactory) {
+        this.topicFactory = topicFactory;
+    }
+
+    /**
+     * Invoked when a message is received.
+     * Implementations are expected to take the message,
+     * 1) queue it if necessary,
+     * 2) get the topic from the message
+     * 3) invoke the message subscribers for the topic that are registered in the
+     * associated {@link com.fillta.higgs.EventProcessor}.
+     *
+     * @param ctx the channel context
+     * @param msg the message to queue
+     */
+    public abstract void enqueue(ChannelHandlerContext ctx, IM msg);
+
+    /**
+     * Subscribes a function/callback to the given topic
+     *
+     * @param topic    the topic to listen to
+     * @param function the callback to be invoked
+     */
+    public void listen(T topic, Function1<ChannelMessage<IM>> function) {
+        messageSubscribers.put(topic, function);
+    }
+
+    /**
+     * Subscribes the given function to <em>all</em> messages/events.
+     *
+     * @param function
+     */
+    public void listen(Function1<ChannelMessage<IM>> function) {
+        allMessageSubscribers.add(function);
+    }
+
+    /**
+     * @param topic the topic to check
+     * @return true if at least one function is subscribed to the given topic
+     */
+    public boolean listening(T topic) {
+        return messageSubscribers.containsKey(topic);
+    }
+
+    /**
+     * @param topic Removes all functions subscribed to the given topic
+     * @return the list of removed functions
+     */
+    public List<Function1<ChannelMessage<IM>>> removeAll(T topic) {
+        return messageSubscribers.removeAll(topic);
+    }
+
+    /**
+     * Invoke all listeners to the given message on the current thread.
+     *
+     * @param ctx
+     * @param msg
+     */
+    public void invokeListeners(ChannelHandlerContext ctx, IM msg) {
+        T topic = topicFactory.extract(msg);
+        ChannelMessage a = new ChannelMessage(ctx, msg);
+        //functions subscribed to "all" messages (note: new ArrayList(collection) copies)
+        List<Function1<ChannelMessage<IM>>> listeners = new ArrayList<>(allMessageSubscribers);
+        for (Function1<ChannelMessage<IM>> function : listeners) {
+            function.call(a);
+        }
+        //now process functions subscribed only to this message's topic
+        listeners = new ArrayList<>(messageSubscribers.get(topic));
+        for (Function1<ChannelMessage<IM>> function : listeners) {
+            function.call(a);
+        }
+        //only incoming messages count
+        if (listeners.size() == 0 && !a.isOutGoing) {
+            log.warn(String.format("Message received and decoded but no listeners found. Topic:%s", topic));
+        }
+    }
+}
---
+++ higgs-core/src/main/java/com/fillta/higgs/queueingStrategies/SameThreadQueueingStrategy.java
@@ -0,0 +1,26 @@
+package com.fillta.higgs.queueingStrategies;
+
+import com.fillta.higgs.MessageTopicFactory;
+import com.fillta.higgs.util.Function1;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class SameThreadQueueingStrategy<T, IM> extends QueueingStrategy<T, IM> {
+    public SameThreadQueueingStrategy(MessageTopicFactory<T, IM> topicFactory) {
+        super(topicFactory);
+    }
+
+    /**
+     * Invokes all {@link Function1}s subscribed to this message's topic on the current thread
+     * without buffering/queueing
+     *
+     * @param ctx the channel context
+     * @param msg the message to queue
+     */
+    @Override
+    public void enqueue(ChannelHandlerContext ctx, IM msg) {
+        invokeListeners(ctx, msg);
+    }
+}
