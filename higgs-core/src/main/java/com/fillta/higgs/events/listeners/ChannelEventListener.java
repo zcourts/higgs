5b874094653b5d937405292437dc4a7a13491e43
Courtney Robinson <courtney@crlog.info>
2012-12-09 04:04:02 +0000

    event listener interface

---
+++ higgs-core/src/main/java/com/fillta/higgs/events/listeners/ChannelEventListener.java
@@ -0,0 +1,15 @@
+package com.fillta.higgs.events.listeners;
+
+import com.google.common.base.Optional;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public abstract class ChannelEventListener {
+    public abstract void triggered(ChannelHandlerContext ctx, Optional<Throwable> ex);
+
+    public boolean consume() {
+        return false;
+    }
+}
