95a8bef3dd8685e56252d87b377402fbca32af9f
Courtney Robinson <courtney@crlog.info>
2012-12-20 00:45:09 +0000

    added method to get all super class fields up to N

--- higgs-core/src/main/java/com/fillta/higgs/reflect/ReflectionUtil.java
+++ higgs-core/src/main/java/com/fillta/higgs/reflect/ReflectionUtil.java
@@ -4,14 +4,21 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 public class ReflectionUtil {
+	private final int MAX_RECURSION_DEPTH;
+
 	public ReflectionUtil() {
+		this(10);
+	}
+
+	public ReflectionUtil(int maxRecursionDepth) {
+		MAX_RECURSION_DEPTH = maxRecursionDepth;
 	}
 
 	public List<Field> getAllFields(List<Field> fields, Class<?> type, int depth) {
 		for (Field field : type.getDeclaredFields()) {
 			fields.add(field);
 		}
-		if (type.getSuperclass() != null && depth <= BosonWriter.MAX_RECURSION_DEPTH) {
+		if (type.getSuperclass() != null && depth <= MAX_RECURSION_DEPTH) {
 			fields = getAllFields(fields, type.getSuperclass(), ++depth);
 		}
 		return fields;
