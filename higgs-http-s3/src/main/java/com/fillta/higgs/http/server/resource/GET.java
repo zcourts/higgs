c267a64cd9aa69eb2c4428a0ba5851807d00e8b8
Courtney Robinson <courtney@crlog.info>
2013-01-03 00:21:31 +0000

    added set of annotation to configure how reosurces are processed

---
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/resource/GET.java
@@ -0,0 +1,14 @@
+package com.fillta.higgs.http.server.resource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface GET {
+}
