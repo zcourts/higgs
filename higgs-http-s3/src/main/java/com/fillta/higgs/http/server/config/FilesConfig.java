e4e68de61c7e0a019276e953f43624c7c2dd4e9d
Courtney Robinson <courtney@crlog.info>
2013-01-03 00:20:04 +0000

    added more config options

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/config/FilesConfig.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/config/FilesConfig.java
@@ -1,5 +1,8 @@
 package com.fillta.higgs.http.server.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
@@ -7,4 +10,12 @@ public class FilesConfig {
 	public boolean delete_temp_on_exit = true;
 	//use default system temp dir if null
 	public String temp_directory = null;
+	//how big are the chunks when sending a file
+	public int chunk_size = 8192;
+	public String public_directory = "public";
+	public boolean enable_directory_listing = true;
+	public boolean serve_index_file = true;
+	public String index_file = "/index.html";
+	//colon separates each, comma, separates multiple extensions
+	public Map<String, String> custom_mime_types = new HashMap<>(); //"htm,html:text/html;json:application/json;xml:application/xml";
 }
