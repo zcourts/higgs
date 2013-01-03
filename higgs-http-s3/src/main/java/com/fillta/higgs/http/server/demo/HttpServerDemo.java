fdd11e32d967b6aeb2d76a2e3abe73656a1f89ea
Courtney Robinson <courtney@crlog.info>
2013-01-03 07:07:40 +0000

    make config generic

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/demo/HttpServerDemo.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/demo/HttpServerDemo.java
@@ -3,6 +3,7 @@ package com.fillta.higgs.http.server.demo;
 import com.fillta.higgs.events.HiggsEvent;
 import com.fillta.higgs.events.listeners.ChannelEventListener;
 import com.fillta.higgs.http.server.HttpServer;
+import com.fillta.higgs.http.server.config.ServerConfig;
 import com.google.common.base.Optional;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ import java.io.IOException;
  */
 public class HttpServerDemo {
 	public static void main(String... args) throws IOException, InterruptedException {
-		HttpServer server = new HttpServer("./config.yml");
+		HttpServer server = new HttpServer(ServerConfig.class,"./config.yml");
 		server.on(HiggsEvent.EXCEPTION_CAUGHT, new ChannelEventListener() {
 			public void triggered(final ChannelHandlerContext ctx, final Optional<Throwable> ex) {
 				if (ex.isPresent()) {
