a460459583a7db5afb8ecc8803f7146d1c415b4b
Courtney Robinson <courtney@crlog.info>
2013-01-03 00:14:53 +0000

    simple status extension

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/HttpStatus.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/HttpStatus.java
@@ -1,7 +1,16 @@
 package com.fillta.higgs.http.server;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class HttpStatus {
+public class HttpStatus extends HttpResponseStatus {
+	/**
+	 * Creates a new instance with the specified {@code code} and its
+	 * {@code reasonPhrase}.
+	 */
+	public HttpStatus(int code, String reasonPhrase) {
+		super(code, reasonPhrase);
+	}
 }
