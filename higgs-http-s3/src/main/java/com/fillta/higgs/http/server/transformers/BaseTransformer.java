c781f2ff58812694edeaccd49a4069091016119b
Courtney Robinson <courtney@crlog.info>
2013-01-03 00:41:32 +0000

    base implementation of the response transformer itnerface

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/transformers/BaseTransformer.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/transformers/BaseTransformer.java
@@ -1,7 +1,31 @@
 package com.fillta.higgs.http.server.transformers;
 
+import com.fillta.higgs.http.server.HttpRequest;
+import com.fillta.higgs.http.server.HttpResponse;
+import com.fillta.higgs.http.server.HttpServer;
+import com.fillta.higgs.http.server.ResponseTransformer;
+
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class BaseTransformer {
+public abstract class BaseTransformer implements ResponseTransformer {
+	public HttpResponse tryNextTransformer(final HttpServer server, Object returns, HttpRequest request,
+	                                       final Queue<ResponseTransformer> transformers) {
+		//copy the transformer queue so removing an item doesn't remove it from the server
+		LinkedBlockingDeque<ResponseTransformer> copied = new LinkedBlockingDeque<>(transformers);
+		copied.remove(this);
+		Iterator<ResponseTransformer> it = copied.descendingIterator();
+		while (it.hasNext()) {
+			ResponseTransformer transformer = it.next();
+			if (transformer != this && transformer.canTransform(returns, request)) {
+				transformer.transform(server, returns, request, copied);
+			}
+		}
+		return null;
+	}
+
 }
