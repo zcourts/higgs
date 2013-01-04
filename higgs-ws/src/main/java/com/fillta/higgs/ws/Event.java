11dbf892ecf37eab33d7743a9d72160c1c216fac
Courtney Robinson <courtney@crlog.info>
2013-01-04 03:08:42 +0000

    generic web socket event. not currently used

--- higgs-ws/src/main/java/com/fillta/higgs/ws/Event.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/Event.java
@@ -1,7 +1,20 @@
 package com.fillta.higgs.ws;
 
+import io.netty.buffer.ByteBuf;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class Event {
+public interface Event<T, M> {
+	public T getTopic();
+
+	public void setTopic(T topic);
+
+	public M getMessage();
+
+	public void setMessage(M data);
+
+//	public ByteBuf serialize();
+//
+//	public <T> T from(Class<T> type);
 }
