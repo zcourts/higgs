e6eb5eb56569397230c0dd30c9a6244622b59ead
Courtney Robinson <courtney@crlog.info>
2013-01-04 03:10:10 +0000

    flash socket protocol encoder and decoders

--- higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Decoder.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Decoder.java
@@ -1,7 +1,24 @@
 package com.fillta.higgs.ws.flash;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class Decoder {
+public class Decoder extends ByteToMessageDecoder<ByteBuf> {
+	protected ByteBuf decode(final ChannelHandlerContext context, final ByteBuf buf) throws Exception {
+		//first 3 bytes is header
+		//next 4 bytes is the message size
+		//everything after is the string payload
+		if (buf.readableBytes() < 7)
+			return null;
+		ByteBuf header = buf.readBytes(3); //get header and increase reader index by 3
+		int size = buf.readBytes(4).readInt();//get the message size
+		if (buf.readableBytes() < size) //if the entire message isn't available yet, wait...
+			return null;
+		//read the entire message, without the header or size and return it
+		return buf.readBytes(size);
+	}
 }
--- higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Encoder.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Encoder.java
@@ -1,7 +1,22 @@
 package com.fillta.higgs.ws.flash;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class Encoder {
+public class Encoder extends MessageToByteEncoder<ByteBuf> {
+	protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf buf) throws Exception {
+		//first 3 bytes is header
+		//next 4 bytes is the message size
+		//everything after is the string payload
+		buf.writeByte('H');
+		buf.writeByte('F');
+		buf.writeByte('S'); //header written
+		buf.writeInt(msg.writerIndex());     //message size written
+		buf.writeBytes(buf);  //data written
+		ctx.flush();     //flush
+	}
 }
