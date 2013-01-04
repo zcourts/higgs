b32e78053f3002c2a966b1e1c5c0d07cc9dd5af1
Courtney Robinson <courtney@crlog.info>
2013-01-04 03:08:54 +0000

    JSON event object

--- higgs-ws/src/main/java/com/fillta/higgs/ws/JsonEvent.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/JsonEvent.java
@@ -1,13 +1,7 @@
 package com.fillta.higgs.ws;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 /**
  * A simple web socket event with a topic and message property.
@@ -16,13 +10,12 @@ import java.io.IOException;
  *
  * @author Courtney Robinson <courtney@crlog.info>
  */
-public class JsonEvent implements Event<String, ByteBuf> {
+public class JsonEvent implements Event<String, Object> {
 	private final Logger log = LoggerFactory.getLogger(getClass());
-	private final static ObjectMapper mapper = new ObjectMapper();
-	private ByteBuf message;
+	private Object message;
 	private String topic = "";
 
-	public JsonEvent(final String topic, ByteBuf obj) {
+	public JsonEvent(String topic, Object obj) {
 		this.topic = topic;
 		message = obj;
 	}
@@ -30,7 +23,7 @@ public class JsonEvent implements Event<String, ByteBuf> {
 	public JsonEvent() {
 	}
 
-	public JsonEvent(final String topic) {
+	public JsonEvent(String topic) {
 		this.topic = topic;
 	}
 
@@ -38,36 +31,16 @@ public class JsonEvent implements Event<String, ByteBuf> {
 		return topic;
 	}
 
-	public void setTopic(final String topic) {
+	public void setTopic(String topic) {
 		this.topic = topic;
 	}
 
-	public ByteBuf getMessage() {
+	public Object getMessage() {
 		return message;
 	}
 
-	public void setMessage(ByteBuf data) {
+	public void setMessage(Object data) {
 		message = data;
-	}
-
-	public ByteBuf serialize() {
-		try {
-			return Unpooled.wrappedBuffer(mapper.writeValueAsBytes(this));
-		} catch (JsonProcessingException e) {
-			log.warn(String.format("Failed to serialize text event %s", toString()));
-			return Unpooled.buffer();
-		}
-	}
-
-	public <T> T from(Class<T> type) {
-		try {
-			byte[] data = new byte[message.writerIndex()];
-			message.readBytes(data);
-			return mapper.readValue(data, type);
-		} catch (IOException e) {
-			log.warn(String.format("Unable to de-serialize message from {%s}", type.getName()));
-			return null;
-		}
 	}
 
 	public String toString() {
