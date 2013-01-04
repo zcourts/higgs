87f0f7a69acacbf7c7bf82d393b1ef09bb062047
Courtney Robinson <courtney@crlog.info>
2013-01-04 03:05:05 +0000

    add default mime types

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/config/FilesConfig.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/config/FilesConfig.java
@@ -7,6 +7,15 @@ import java.util.Map;
  * @author Courtney Robinson <courtney@crlog.info>
  */
 public class FilesConfig {
+	public FilesConfig() {
+		//add some default mime types
+		custom_mime_types.put("htm,html", "text/html");
+		custom_mime_types.put("json", "application/json");
+		custom_mime_types.put("xml", "application/xml");
+		custom_mime_types.put("png", "image/png");
+		custom_mime_types.put("css", "text/css");
+	}
+
 	public boolean delete_temp_on_exit = true;
 	//use default system temp dir if null
 	public String temp_directory = null;
