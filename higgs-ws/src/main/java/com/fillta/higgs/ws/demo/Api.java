cb5c6cb7c8d72e2d0bee2ba66da895b31a4b5646
Courtney Robinson <courtney@crlog.info>
2013-01-04 03:09:47 +0000

    simple websocket demo

--- higgs-ws/src/main/java/com/fillta/higgs/ws/demo/Api.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/demo/Api.java
@@ -1,7 +1,16 @@
 package com.fillta.higgs.ws.demo;
 
+import com.fillta.higgs.http.server.resource.GET;
+import com.fillta.higgs.http.server.resource.Path;
+
 /**
  * @author Courtney Robinson <courtney@crlog.info>
  */
+@Path("/")
 public class Api {
+	@Path("/test")
+	@GET
+	public String test() {
+		return "{}";
+	}
 }
