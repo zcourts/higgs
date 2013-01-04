af64182946fd83cd448f8399eb325e354f3a55d1
Courtney Robinson <courtney@crlog.info>
2013-01-04 12:59:10 +0000

    add flash socket policy detector, encoder and decoder

--- higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Encoder.java
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/flash/Encoder.java
@@ -8,13 +8,15 @@ import io.netty.handler.codec.MessageToByteEncoder;
  * @author Courtney Robinson <courtney@crlog.info>
  */
 public class Encoder extends MessageToByteEncoder<ByteBuf> {
+	public static final int H = 72, F = 83, S = 70;
+
 	protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf buf) throws Exception {
-		//first 3 bytes is header
+		//first 3 bytes is header   ('H'=72,'F'=83,'S'=70)
 		//next 4 bytes is the message size
 		//everything after is the string payload
-		buf.writeByte('H');
-		buf.writeByte('F');
-		buf.writeByte('S'); //header written
+		buf.writeByte(H);
+		buf.writeByte(F);
+		buf.writeByte(S); //header written
 		buf.writeInt(msg.writerIndex());     //message size written
 		buf.writeBytes(buf);  //data written
 		ctx.flush();     //flush
---
+++ higgs-ws/src/main/java/com/fillta/higgs/ws/flash/FlashPolicyEncoder.java
@@ -0,0 +1,18 @@
+package com.fillta.higgs.ws.flash;
+
+import com.fillta.higgs.ws.FlashPolicyFile;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class FlashPolicyEncoder extends MessageToByteEncoder<FlashPolicyFile> {
+	protected void encode(ChannelHandlerContext context, FlashPolicyFile s, ByteBuf buf) throws Exception {
+		buf.writeBytes(s.getBytes());
+		//write NULL byte, 0x00
+		buf.writeByte(0x00);
+		context.flush();
+	}
+}
