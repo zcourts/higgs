094f07d44051d9084bc04e84a9490b09b3a8b988
Courtney Robinson <courtney@crlog.info>
2013-01-25 11:29:38 +0000

    add experimental built in support for OAuth v1

---
+++ higgs-http-client/src/main/java/com/fillta/higgs/http/client/oauth/OAuthException.java
@@ -0,0 +1,10 @@
+package com.fillta.higgs.http.client.oauth;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class OAuthException extends RuntimeException {
+	public OAuthException(final String msg, final Throwable cause) {
+		super(msg, cause);
+	}
+}
