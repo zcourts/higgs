14ad05e7e25b47049e73a8c8027097d340ade5a2
Courtney Robinson <courtney@crlog.info>
2013-01-25 17:18:57 +0000

    remove unnecessary method & only set non-final fields

--- higgs-http-s3/src/main/java/com/fillta/higgs/http/server/params/HttpCookie.java
+++ higgs-http-s3/src/main/java/com/fillta/higgs/http/server/params/HttpCookie.java
@@ -3,11 +3,11 @@ package com.fillta.higgs.http.server.params;
 import com.fillta.higgs.reflect.ReflectionUtil;
 import io.netty.handler.codec.http.Cookie;
 import io.netty.handler.codec.http.DefaultCookie;
-import io.netty.handler.codec.http.DefaultHttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,11 +25,13 @@ public class HttpCookie extends DefaultCookie {
 
     public HttpCookie(Cookie cookie) {
         this(cookie.getName(), cookie.getValue());
-        List<Field> fields = reflection.getAllFields(new ArrayList<Field>(), DefaultHttpRequest.class, 10);
+        List<Field> fields = reflection.getAllFields(new ArrayList<Field>(), DefaultCookie.class, 1);
         for (Field field : fields) {
             try {
-                field.setAccessible(true);
-                field.set(this, field.get(cookie));
+                if (!Modifier.isFinal(field.getModifiers())) {
+                    field.setAccessible(true);
+                    field.set(this, field.get(cookie));
+                }
             } catch (Throwable t) {
                 log.warn("Error copying cookie field", t);
             }
