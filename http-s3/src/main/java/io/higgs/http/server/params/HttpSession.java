a55b9617931782a7063f1b9ea874a350f2f06135
zcourts <courtney@crlog.info>
2013-05-10 22:53:37 +0100

    add flash session objects that auto delete after first get

--- http-s3/src/main/java/io/higgs/http/server/params/HttpSession.java
+++ http-s3/src/main/java/io/higgs/http/server/params/HttpSession.java
@@ -6,7 +6,32 @@ import java.util.HashMap;
  * @author Courtney Robinson <courtney@crlog.info>
  */
 public class HttpSession<K, V> extends HashMap<K, V> {
+    private final HashMap<K, V> flash = new HashMap<>();
+
+    /**
+     * Adds a key value pair to the session which is good for one use.
+     * Once the object is retrieved it is automatically removed
+     */
+    public V flash(K key, V value) {
+        return flash.put(key, value);
+    }
+
+    @Override
+    public V get(Object key) {
+        V val = super.get(key);
+        if (val == null) {
+            val = flash.get(key);
+            if (val != null) {
+                flash.remove(key);
+            }
+        }
+        return val;
+    }
+
+    /**
+     * @return Total combination of normal session objects and flash objects
+     */
     public int getSize() {
-        return size();
+        return size() + flash.size();
     }
 }
