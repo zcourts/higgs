322680dcce272dfc1a4165a8e420066c609afdff
zcourts <courtney@crlog.info>
2013-05-19 17:34:51 +0100

    line by line, entire pages or files methods added for reading http response

---
+++ http-client/src/main/java/io/higgs/http/client/future/FileReader.java
@@ -0,0 +1,46 @@
+package io.higgs.http.client.future;
+
+import io.netty.buffer.ByteBuf;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class FileReader extends Reader<File> {
+    protected File file;
+    protected FileOutputStream out;
+
+    public FileReader() throws IOException {
+        this(null);
+    }
+
+    public FileReader(Function<File> function) throws IOException {
+        if (function != null) {
+            listen(function);
+        }
+        file = Files.createTempFile("higgs-http-client-temp-" + new Double(Math.random()).longValue(), ".tmp").toFile();
+        out = new FileOutputStream(file);
+    }
+
+    @Override
+    public void data(ByteBuf data) {
+        byte[] tmp = new byte[data.readableBytes()];
+        data.readBytes(tmp);
+        try {
+            out.write(tmp);
+        } catch (IOException e) {
+            log.warn("Error writing data to file", e);
+        }
+    }
+
+    @Override
+    public void done() {
+        for (Function<File> function : functions) {
+            function.apply(file);
+        }
+    }
+}
