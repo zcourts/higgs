e4d5acaaf65ac713cb62c1c9220847c7b2fcffd7
zcourts <courtney@crlog.info>
2014-04-05 17:06:54 +0100

    file utility that will be used to resolve file for the system
    See http://docs.oracle.com/javase/tutorial/deployment/jar/apiindex.html
    
    http://docs.oracle.com/javase/tutorial/deployment/jar/jarclassloader.htm
    l
    
    the Paths.xxx API and others

---
+++ core/src/main/java/io/higgs/core/FileUtil.java
@@ -0,0 +1,15 @@
+package io.higgs.core;
+
+import io.higgs.core.files.ResolvedFile;
+
+/**
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class FileUtil {
+    protected FileUtil() {
+    }
+
+    public static ResolvedFile resolve(String path) {
+        return null;
+    }
+}
---
+++ core/src/main/java/io/higgs/core/files/ResolvedFile.java
@@ -0,0 +1,10 @@
+package io.higgs.core.files;
+
+/**
+ * Represents a file from the underlying file system.
+ * The source of the file can be the disk file system directly or from within a JAR file.
+ *
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+public class ResolvedFile {
+}
