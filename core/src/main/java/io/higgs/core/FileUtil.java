d9314f75697885f770c99f0dd3877784f82858f4
zcourts <courtney@crlog.info>
2014-04-06 11:23:55 +0100

    added some more docs/comments

--- core/src/main/java/io/higgs/core/FileUtil.java
+++ core/src/main/java/io/higgs/core/FileUtil.java
@@ -9,6 +9,13 @@ public class FileUtil {
     protected FileUtil() {
     }
 
+    /**
+     * Given a path, try locating the file/directory represented by it.
+     * First check the file system, then check within the current JAR.
+     * In both cases the path must be a child of the base directory provided.
+     * @param path
+     * @return A resolved file or raise a file not found exception
+     */
     public static ResolvedFile resolve(String path) {
         return null;
     }
