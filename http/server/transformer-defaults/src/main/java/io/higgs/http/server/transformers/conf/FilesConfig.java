6d3912280522f892a055003a5744a8bdbd2a91eb
zcourts <courtney@crlog.info>
2014-04-07 23:42:41 +0100

    additional module configs

--- http/server/transformer-defaults/src/main/java/io/higgs/http/server/transformers/conf/FilesConfig.java
+++ http/server/transformer-defaults/src/main/java/io/higgs/http/server/transformers/conf/FilesConfig.java
@@ -1,4 +1,4 @@
-package io.higgs.http.server.config;
+package io.higgs.http.server.transformers.conf;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,12 +12,9 @@ public class FilesConfig {
     public String temp_directory;
     //how big are the chunks when sending a file
     public int chunk_size = 8192;
-    public String public_directory = "public";
-    public boolean enable_directory_listing = true;
-    public boolean serve_index_file = true;
-    public String index_file = "/index.html";
     //colon separates each, comma, separates multiple extensions
     public Map<String, String> custom_mime_types = new HashMap<>();
+    public int priority = -1;
 
     public FilesConfig() {
         //add some default mime types
