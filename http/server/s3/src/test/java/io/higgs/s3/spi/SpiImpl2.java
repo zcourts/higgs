743b838ad239e3961b58bd0cabbb7735af25cdb1
zcourts <courtney@crlog.info>
2014-05-16 19:24:19 -0700

    actual spi test files

---
+++ http/server/s3/src/test/java/io/higgs/s3/spi/SpiImpl2.java
@@ -0,0 +1,13 @@
+package io.higgs.s3.spi;
+
+import io.higgs.common.annotations.tests.SpiAnnotation;
+import io.higgs.common.annotations.tests.SpiInterface;
+
+/**
+ * Generate SPI entry for SpiInterface
+ *
+ * @author Courtney Robinson <courtney@crlog.info>
+ */
+@SpiAnnotation
+public class SpiImpl2 implements SpiInterface {
+}
