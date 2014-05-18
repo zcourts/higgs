42748a9cbf8a7ed697f2443cad35396998823658
zcourts <courtney@crlog.info>
2014-05-17 22:14:45 -0700

    write against provider annotation directly instead of using samples

--- http/common/src/main/java/io/higgs/common/annotations/tests/SpiInterface.java
+++
@@ -1,8 +0,0 @@
-package io.higgs.common.annotations.tests;
-
-/**
- * @author Courtney Robinson <courtney@crlog.info>
- */
-@SpiAnnotation
-public interface SpiInterface {
-}
--- http/common/src/main/java/io/higgs/common/annotations/tests/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Defines a few utility classes, interfaces and annotations used for testing SPI.
- * NOT FOR GENERAL USE!
- */
-package io.higgs.common.annotations.tests;
